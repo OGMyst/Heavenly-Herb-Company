9ffbff57aa5f288ff4ff2702143a2bcf340ff9c3
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2020-11-22 20:54:26 +0000

    Added stripe elements to checkout

--- checkout/views.py
+++ checkout/views.py
@@ -5,6 +5,7 @@ from .forms import OrderForm
 
 
 def checkout(request):
+    stripe_public_key = 'pk_test_51HPD0FAf1PaReHG9eggRTWtYTEwcMeG8DwPDKVDckA71M4KzbQGeUuU5pQc0RtL43juEp3e0KvTWKZaMNr8OYMJH004mhvS2uM'
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
@@ -14,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
