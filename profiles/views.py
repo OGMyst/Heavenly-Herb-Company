5709192e7b5379be7402a597fc30fdf2c0a1ec39
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2020-11-28 15:45:44 +0000

    created userprofile model. Added it to order model. created view for profile template

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def profile(request):
+    """ A view to return the index page """
+
+    return render(request, 'profile/profile.html')
