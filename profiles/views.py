9d2cffc9b23605a9e437fb8fd44f2b84d6931558
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2020-12-27 15:18:29 +0000

    Commented out webhook handler code and pasted in simple version of the code to try and find issue. Webhooks now being received by stripe.

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileGeneralForm
 
 
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    Generalform = UserProfileGeneralForm(instance=profile)
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'general_form': Generalform,
     }
 
     return render(request, template, context)
