9be00cc3daf5ccc4ba5d28e8336c7cf9b72fd4a4
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2020-12-30 17:24:39 +0000

    Added address form for profile page

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from .models import UserProfile
-from django.contrib.auth.models import User
 
 
 class UserProfileGeneralForm(forms.ModelForm):
@@ -21,6 +20,37 @@ class UserProfileGeneralForm(forms.ModelForm):
         }
 
         self.fields['phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'profile-form-input'
+            self.fields[field].label = False
+
+
+class UserProfileAddressForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('default_street_address1',
+                  'default_street_address2',
+                  'default_town_or_city',
+                  'default_county',
+                  'default_postcode')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_town_or_city': 'Town or City',
+            'default_county': 'County, State or Locality',
+            'default_postcode': 'Postal Code',
+        }
+
+        self.fields['default_street_address1'].widget.attrs['autofocus'] = True
         for field in self.fields:
             placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
-from .forms import UserProfileGeneralForm
+from .forms import UserProfileGeneralForm, UserProfileAddressForm
 
 
 def profile(request):
@@ -13,9 +13,20 @@ def profile(request):
                     'email': profile.user.email,
                     'phone_number': profile.default_phone_number,
     })
+
+    Addressform = UserProfileAddressForm(initial={
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'town_or_city': profile.default_town_or_city,
+                    'county': profile.default_county,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+    })
+
     template = 'profiles/profile.html'
     context = {
         'general_form': Generalform,
+        'address_form': Addressform
     }
 
     return render(request, template, context)
