0965cdf31ebbaaf6bbbd4e9a3f19b596b8240402
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2021-02-06 10:49:44 +0000

    Made some style and small layout changes to messages

--- bag/views.py
+++ bag/views.py
@@ -25,6 +25,8 @@ def add_to_bag(request, item_id):
                          f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
+        messages.success(request,
+                         f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
 
