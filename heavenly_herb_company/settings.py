6b0265e04472e93c6c9285ec51660e3d8fc90682
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2021-02-23 20:34:27 +0000

    resolved import error with django-storages

--- heavenly_herb_company/settings.py
+++ heavenly_herb_company/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#+@v05b#230x=5k^-67m-b8^&8z4fpkuk(ty1hlagqr=uuw_&c'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -178,12 +178,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-
-    # Cache control
-    AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
-    }
 
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'heavenly-herb-company'
