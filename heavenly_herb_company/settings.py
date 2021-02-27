7f5be605d715191fb00eda99f4a0053d4958500a
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2021-02-27 17:05:47 +0000

    Removed secret key. About to redact all sensitive information from repo history

--- heavenly_herb_company/settings.py
+++ heavenly_herb_company/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#+@v05b#230x=5k^-67m-b8^&8z4fpkuk(ty1hlagqr=uuw_&c'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
