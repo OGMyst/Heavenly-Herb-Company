78e92a7bb48b2628096839d2ec6f02b2fff3ee28
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2021-02-28 16:22:46 +0000

    Added cache control for AWS

--- heavenly_herb_company/settings.py
+++ heavenly_herb_company/settings.py
@@ -121,15 +121,6 @@ WSGI_APPLICATION = 'heavenly_herb_company.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 if 'DATABASE_URL' in os.environ:
     print("Connected to Postgres")
     DATABASES = {
@@ -188,6 +179,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
 
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'heavenly-herb-company'
