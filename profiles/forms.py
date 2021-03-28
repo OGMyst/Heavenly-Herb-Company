093b41eeeb927282a5059f0fe7cec1b650da6ced
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2021-03-28 14:21:08 +0000

    Removed UserGeneralForm since it is no longer going to be used

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,30 +1,5 @@
 from django import forms
-from .models import UserProfile, Address
-
-
-class UserGeneralForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ('full_name', 'email', 'phone_number')
-
-    def __init__(self, *args, **kwargs):
-        """
-        Add placeholders and classes, remove auto-generated
-        labels and set autofocus on first field
-        """
-        super().__init__(*args, **kwargs)
-        placeholders = {
-            'full_name': 'Full Name',
-            'email': 'Email Address',
-            'phone_number': 'Phone Number',
-        }
-
-        self.fields['phone_number'].widget.attrs['autofocus'] = True
-        for field in self.fields:
-            placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'profile-form-input'
-            self.fields[field].label = False
+from .models import Address
 
 
 class AddressForm(forms.ModelForm):
