d87be96f49d9ab02d636272edfda5959678d0515
Hugh Padmore <hughwiliampadmore@hotmail.co.uk>
2021-03-30 17:28:19 +0000

    Small fixes to improve display on desktop chrome browser. Removed Allauth social account templates

--- contact_us/views.py
+++ contact_us/views.py
@@ -15,12 +15,12 @@ def contact_us(request):
             contact_us = contact_form.save(commit=False)
             contact_us.save()
             messages.success(request, 'Thank you for your message.' +
-                             'We will be in touch as soon as we can.')
+                             ' We will be in touch as soon as we can.')
         else:
             messages.error(request, 'Something went wrong when processing' +
-                           'your message. Please check you have filled in' +
-                           'all the fields and that your email address is' +
-                           'correct')
+                           ' your message. Please check you have filled in' +
+                           ' all the fields and that your email address is' +
+                           ' correct')
 
     context = {
             'contact_form': contact_form
